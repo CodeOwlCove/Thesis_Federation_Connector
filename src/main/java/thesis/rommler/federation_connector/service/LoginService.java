2d0b952d67c33908f9090d233b6ec8de228a5c25
rommler <Luca_Hendrik_Martin.Rommler@Student.Reutlingen-University.DE>
2024-01-18 17:20:44 +0100

    Fixed bugs in Login

--- src/main/java/thesis/rommler/federation_connector/service/LoginService.java
+++ src/main/java/thesis/rommler/federation_connector/service/LoginService.java
@@ -1,37 +1,55 @@
 package thesis.rommler.federation_connector.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 import static java.lang.System.exit;
 
 @Service
 public class LoginService {
 
+    private Logger logger = Logger.getLogger(LoginService.class.getName());
     private RestTemplate restTemplate;
+
+    @Autowired
+    private Environment environment;
 
     public LoginService(RestTemplate restTemplate){
         this.restTemplate = restTemplate;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        logger.info("- Application started.");
         LogIn();
     }
 
     private void LogIn(){
-        String apiUrl = "http://localhost:9080/login?requester_ip=localhost&requester_port=7080&socket_port=10080";
+        var serverPort = Integer.parseInt(environment.getProperty("server.port"));
+        var hostName = environment.getProperty("server.address", "localhost");
+
+        String apiUrl = "http://localhost:9080/login?requester_ip="+hostName+"&requester_port="+serverPort+"&socket_port=10080";
 
         try {
             // Make a GET request and handle the response
             String response = restTemplate.getForObject(apiUrl, String.class);
 
             if(response.equals("ok"))
-                System.out.println("Logged in successfully.");
+                logger.info("- Logged in successfully.");
             else {
-                System.out.println("Error while logging in.");
+                logger.severe("- Error while logging in.");
                 exit(-1);
             }
 
         }catch (Exception e){
-            System.out.println("Error while logging in: " + e.getMessage());
+            logger.severe("- Error while logging in: " + e.getMessage());
             exit(-1);
         }
     }
