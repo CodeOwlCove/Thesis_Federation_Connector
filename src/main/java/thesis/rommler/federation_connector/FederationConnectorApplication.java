d8dd462bce1470cfe073ab7950414a43a0cfbffd
rommler <luca_hendrik_martin.rommler@student.reutlingen-university.de>
2024-02-03 18:26:24 +0100

    Push for work

--- src/main/java/thesis/rommler/federation_connector/FederationConnectorApplication.java
+++ src/main/java/thesis/rommler/federation_connector/FederationConnectorApplication.java
@@ -2,8 +2,6 @@ package thesis.rommler.federation_connector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class FederationConnectorApplication {
